5780298273d5d44a68dfabb56adf47d4a0f01ea5
Kamal Nayan <geniusamansingh@gmail.com>
2023-06-24 13:45:11 +0000

    aurora estate setup

---
+++ const/contractAddresses.ts
@@ -0,0 +1,16 @@
+/** Replace the values below with the addresses of your smart contracts. */
+
+// 1. Set up the network your smart contracts are deployed to.
+// First, import the chain from the package, then set the NETWORK variable to the chain.
+import { Mumbai } from "@thirdweb-dev/chains";
+export const NETWORK = Mumbai;
+
+// 2. The address of the marketplace V3 smart contract.
+// Deploy your own: https://thirdweb.com/thirdweb.eth/MarketplaceV3
+export const MARKETPLACE_ADDRESS = "";
+
+// 3. The address of your NFT collection smart contract.
+export const NFT_COLLECTION_ADDRESS = "";
+
+// (Optional) Set up the URL of where users can view transactions on
+export const ETHERSCAN_URL = "https://mumbai.polygonscan.com";
